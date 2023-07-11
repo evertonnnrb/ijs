d440c00d77cb6323bf5594e24310823eb510754d
Everton <everton.nnrb@gmail.com>
2023-07-10 20:29:35 -0400

    start

---
+++ src/main/java/com/br/ijs/ijs/IjsApplication.java
@@ -0,0 +1,13 @@
+package com.br.ijs.ijs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ijs/ijs/IjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ijs.ijs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
