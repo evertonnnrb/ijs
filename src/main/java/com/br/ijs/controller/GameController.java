29f3e5b9f94e83cacc9034c4f950442850331e03
Everton <everton.nnrb@gmail.com>
2023-07-11 19:38:11 -0400

    update readme

--- src/main/java/com/br/ijs/controller/GameController.java
+++ src/main/java/com/br/ijs/controller/GameController.java
@@ -4,6 +4,7 @@ import com.br.ijs.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class GameController {
     @GetMapping
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok().body(gameService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(gameService.findById(id));
     }
 }
--- src/main/java/com/br/ijs/service/GameService.java
+++ src/main/java/com/br/ijs/service/GameService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -16,6 +17,9 @@ public class GameService {
 
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
 
+    public Optional<Game> findById(Long id){
+        return gameRepository.findById(id);
     }
 }
